050bea5f1f655f249da72c4d52f8679099ef5300
CardyRGB <cardyrgb@gmail.com>
2026-09-07 15:56:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Copying files..."
-sudo cp ./CachyMac /usr/share/plymouth/themes/
+sudo cp -r ./CachyMac /usr/share/plymouth/themes/
 sleep 0.1
 echo "Done!"
 echo "Setting it as the default Plymouth theme..."
